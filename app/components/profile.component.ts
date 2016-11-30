96cfae5541a596f54728c5b8f32b5296560ed12b
Denis Michailov <denis.michailov@gmail.com>
2016-11-30 15:55:44 +1000

    live search on github

--- app/components/profile.component.ts
+++ app/components/profile.component.ts
@@ -1,21 +1,35 @@
 import { Component } from '@angular/core';
-import { GithubService } from '../services/github.service'
-
+import { GithubService } from '../services/github.service';
 
 @Component({
-	// moduleId: module.id,
-  selector: 'profile',
-  templateUrl: `/app/components/profile.component.html`,
+	moduleId: module.id,
+	selector: 'profile',
+	templateUrl: `/app/components/profile.component.html`
 })
 
 export class ProfileComponent  {
 
-	user: any[];
+	user: any;
+	repos: any[];
+	username: string;
 
 	constructor(private _githubService: GithubService) {
+		this.user = false;
+	}
+
+	searchUser() {
+		this._githubService.updateUser(this.username);
 		this._githubService.getUser().subscribe(user => {
 			console.log(user);
 			this.user = user;
-		})
+		});
+
+		this._githubService.getRepos().subscribe(repos => {
+			console.log(repos);
+			console.log(repos.length);
+			this.repos = repos;
+		});
+
 	}
+
 }
